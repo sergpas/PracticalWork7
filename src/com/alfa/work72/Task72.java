3913465774752112b75e40e1e2e6e68683d40e5f
Admin <sergpas@kil.com>
2021-02-17 21:20:40 +0200

    Work

---
+++ src/com/alfa/work72/Task72.java
@@ -0,0 +1,16 @@
+package com.alfa.work72;
+
+public class Task72 {
+    public static void main(String[] args) {
+        int[] mas = {1,2,3,4,5,6,7,8,9,10};
+        System.out.printf("Sum Odd Elements: " + sumOddElementsArray(mas));
+    }
+    public static int sumOddElementsArray(int[] array){
+        int sumOdd=0;
+        for(int i = 0; i < array.length; i++){
+            if(array[i] < 0) return -1;
+            if( i % 2 == 0) sumOdd+=array[i];
+        }
+        return sumOdd;
+    }
+}

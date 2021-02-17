3f6b7d278bc7f2251428a45df03b651c8a69a9f7
Admin <sergpas@kil.com>
2021-02-17 20:56:16 +0200

    Work

---
+++ src/com/alfa/work71/Task71.java
@@ -0,0 +1,20 @@
+package com.alfa.work71;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Task71 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Massive Size: ");
+        int masSize = scan.nextInt();
+        int[] mas = createArray(masSize);
+        System.out.printf("Massive: "+Arrays.toString(mas));
+    }
+    public static int[] createArray(int size){
+        int[] mas = new int[size];
+        for(int i = 0; i < size; i++){
+            mas[i] = (i+1)*2;
+        }
+        return mas;
+    }
+}

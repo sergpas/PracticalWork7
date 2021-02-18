34e309fc2982320410b9383f8ef9026d9ea0bf7d
Admin <sergpas@kil.com>
2021-02-18 11:51:13 +0200

    Work

--- src/com/alfa/work74/Task74.java
+++ src/com/alfa/work74/Task74.java
@@ -1,9 +1,10 @@
 package com.alfa.work74;
+
 import java.util.*;
 import java.lang.*;
 
 public class Task74 {
-    public  static  void  main(String[]  args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Matrix Size: ");
         int matrixSize = scan.nextInt();
@@ -19,33 +20,33 @@ public class Task74 {
         printMatrix(matrix);
     }
 
-    public static void transpositionMatrix(int[][] matrix){
+    public static void transpositionMatrix(int[][] matrix) {
         int matrixSize = matrix.length;
         int transpMatrix[][] = new int[matrixSize][matrixSize];
-        for(int i=0;i<matrixSize;i++){
-            for(int j=0;j<matrixSize;j++) {
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
                 transpMatrix[j][i] = matrix[i][j];
             }
         }
-        for(int i=0;i<matrixSize;i++){
-            for(int j=0;j<matrixSize;j++) {
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
                 matrix[i][j] = transpMatrix[i][j];
             }
         }
 
     }
 
-    public  static int[][] createMatrix(int size){
+    public static int[][] createMatrix(int size) {
         int matrix[][] = new int[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                matrix[i][j] = (int)(Math.random()*50);
+                matrix[i][j] = (int) (Math.random() * 50);
             }
         }
-        return  matrix;
+        return matrix;
     }
 
-    public static void printMatrix(int[][] matrix){
+    public static void printMatrix(int[][] matrix) {
         for (int[] rowMatrix : matrix) {
             System.out.println(Arrays.toString(rowMatrix));
         }

a323f94265ea29f12700b4b5b8a3867e3e36b95c
Admin <sergpas@kil.com>
2021-02-18 11:51:00 +0200

    Work

--- src/com/alfa/work74/Task74.java
+++ src/com/alfa/work74/Task74.java
@@ -1,7 +1,6 @@
 package com.alfa.work74;
-import java.util.Arrays;
+import java.util.*;
 import java.lang.*;
-import java.util.Scanner;
 
 public class Task74 {
     public  static  void  main(String[]  args){

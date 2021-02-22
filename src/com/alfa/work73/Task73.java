bc01cd177b7aaaffd9f3adb070386fc6fc61dd81
Admin <sergpas@kil.com>
2021-02-22 19:35:34 +0200

    Work

--- src/com/alfa/work73/Task73.java
+++ src/com/alfa/work73/Task73.java
@@ -9,16 +9,13 @@ public class Task73 {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Matrix Size: ");
         int matrixSize = scan.nextInt();
-        if (matrixSize < 0) {
-            System.out.printf("Invalid Matrix Size: " + matrixSize);
-            return;
-        }
         for (int[] rowMatrix : createMatrix(matrixSize)) {
             System.out.println(Arrays.toString(rowMatrix));
         }
     }
 
     public static int[][] createMatrix(int size) {
+        if (size < 0) size = 0;
         int matrix[][] = new int[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
